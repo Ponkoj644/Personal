cf72c4b626fe3e34241da57ebcdde4f1c27c27be
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2023-01-11 17:00:17 +0600

    Project Upload

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// All vatiables
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function () {
+  const nameField = document.getElementById("contactName");
+  const emailField = document.getElementById("contactEmail");
+  const messageField = document.getElementById("contactMessage");
+  const errorDiv = document.getElementById("error-div");
+  console.log(errorDiv);
+
+  // Variable value
+
+  const nameValue = nameField.value;
+  const emailValue = emailField.value;
+  const messagelValue = messageField.value;
+
+  const templateParams = {
+    nameValue: nameValue,
+
+    emailValue: emailValue,
+
+    messagelValue: messagelValue,
+  };
+
+  if (nameValue && emailValue && messagelValue) {
+    console.log(nameValue, emailValue, messagelValue);
+    errorDiv.innerHTML = `<p class="text-success">Your message is send successfully...</p>`;
+    nameField.value = "";
+    emailField.value = "";
+    messageField.value = "";
+
+    emailjs.send(
+      "service_test",
+      "template_35clvva",
+      templateParams,
+      "QVHtjkDXeb1h_XzvC"
+    );
+  } else {
+    errorDiv.innerHTML = `<p class="text-danger">Please fill all required field !</p>`;
+  }
+});
